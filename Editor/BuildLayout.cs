79dd5289fc2de2a1c5788a9aa38c48f120dc5946
pschraut <p-github@console-dev.de>
2021-08-07 10:02:52 +0200

    initial commit, can partially read the buildlayout.txt file

---
+++ Editor/BuildLayout.cs
@@ -0,0 +1,243 @@
+﻿//
+// Addressables Build Layout Explorer for Unity. Copyright (c) 2021 Peter Schraut (www.console-dev.de). See LICENSE.md
+// https://github.com/pschraut/UnityAddressablesBuildLayoutExplorer
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Oddworm.EditorFramework
+{
+    [System.Serializable]
+    public class BuildLayout
+    {
+        public string unityVersion;
+        public string addressablesVersion;
+        public List<Group> groups = new List<Group>();
+
+        [System.Serializable]
+        public class Group
+        {
+            public string name;
+            public string size;
+            public List<Archive> bundles = new List<Archive>();
+        }
+
+        [System.Serializable]
+        public class Archive
+        {
+            public string name;
+            public string size;
+            public List<string> bundleDependencies = new List<string>();
+        }
+
+        public static BuildLayout Load(string path)
+        {
+            var text = System.IO.File.ReadAllText(path);
+            return Parse(text);
+        }
+
+        public static BuildLayout Parse(string text)
+        {
+            var layout = new BuildLayout();
+
+            // Convert text to lines for easier processing
+            var lines = new List<string>();
+            foreach (var line in text.Split(new[] { '\n' }))
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                    lines.Add(line);
+            }
+
+            for (var n = 0; n < lines.Count; ++n)
+            {
+                var line = lines[n];
+
+                if (line.StartsWith("Unity Version:", System.StringComparison.Ordinal))
+                {
+                    layout.unityVersion = line.Substring("Unity Version:".Length).Trim();
+                    continue;
+                }
+
+                if (line.StartsWith("com.unity.addressables:", System.StringComparison.Ordinal))
+                {
+                    layout.addressablesVersion = line.Substring("com.unity.addressables:".Length).Trim();
+                    continue;
+                }
+
+                if (line.StartsWith("Group ", System.StringComparison.Ordinal))
+                {
+                    var group = ReadGroup(ref n);
+                    layout.groups.Add(group);
+                    continue;
+                }
+            }
+
+            // Sort groups alphabetically
+            layout.groups.Sort(delegate (Group a, Group b)
+            {
+                return a.name.CompareTo(b.name);
+            });
+
+            return layout;
+
+            Group ReadGroup(ref int index)
+            {
+                // Group LR-globals (Bundles: 16, Total Size: 128.43KB, Explicit Asset Count: 280)
+                var group = new Group();
+                var groupLine = lines[index];
+                var groupIndend = GetIndend(groupLine);
+
+                // Read the group name
+                var groupName = groupLine;
+                groupName = groupName.Substring(groupName.IndexOf("Group ") + "Group ".Length); // Remove everything before and including "Group"
+                groupName = groupName.Substring(0, groupName.IndexOf(" (Bundles:")); // Remove everything after and including " (Bundles:"
+                groupName = groupName.Trim();
+                group.name = groupName;
+
+                // Read the group size
+                var groupSize = groupLine;
+                groupSize = groupSize.Substring(groupSize.IndexOf("Total Size: ") + "Total Size: ".Length); // Remove everything before and including "Total Size: "
+                groupSize = groupSize.Substring(0, groupSize.IndexOf(", ")); // Remove everything after ", "
+                group.size = groupSize;
+
+                // Iterate over each group line
+                var loopguard = 0;
+                index++;
+                for (; index < lines.Count; ++index)
+                {
+                loop:
+                    if (++loopguard > 30000)
+                    {
+                        Debug.LogError($"loopguard");
+                        break;
+                    }
+
+                    var line = lines[index];
+                    var lineIndend = GetIndend(line);
+                    if (lineIndend <= groupIndend)
+                    {
+                        index--;
+                        return group;
+                    }
+
+                    if (line.StartsWith("\tSchemas"))
+                    {
+                        SkipSchemas(ref index);
+                        goto loop;
+                    }
+
+                    if (line.StartsWith("\tArchive"))
+                    {
+                        var archive = ReadArchive(ref index);
+                        group.bundles.Add(archive);
+                        goto loop;
+                    }
+                }
+
+                return group;
+            }
+
+            void SkipSchemas(ref int index)
+            {
+                var schemasLevel = GetIndend(lines[index]);
+
+                for (index++; index < lines.Count; ++index)
+                {
+                    if (GetIndend(lines[index]) <= schemasLevel)
+                        break;
+                }
+            }
+
+            Archive ReadArchive(ref int index)
+            {
+                var archive = new Archive();
+
+                // Archive lr-globals_assets_assets/configs/balancing_eea0e08713731a51c6aac94795f26fc8.bundle (Size: 10.07KB, Compression: Lz4HC, Asset Bundle Object Size: 1.76KB)
+                var archiveLine = lines[index];
+                var archiveIndend = GetIndend(archiveLine);
+
+                // Read the archive name
+                var archiveName = archiveLine;
+                archiveName = archiveName.Substring(archiveName.IndexOf("Archive") + "Archive".Length); // Remove everything before and including "Archive"
+                archiveName = archiveName.Substring(0, archiveName.IndexOf("(Size:")); // Remove everything after and including "(Size:"
+                archiveName = archiveName.Trim();
+                archive.name = archiveName;
+
+                // Read the archive size
+                var archiveSize = archiveLine;
+                archiveSize = archiveSize.Substring(archiveSize.IndexOf("(Size: ") + "(Size: ".Length); // Remove everything before and including "(Size: "
+                archiveSize = archiveSize.Substring(0, archiveSize.IndexOf(", ")); // Remove everything after ", "
+                archive.size = archiveSize;
+
+
+                var loopguard = 0;
+
+                for (index++; index < lines.Count - 1; ++index)
+                {
+                    if (++loopguard > 30000)
+                    {
+                        Debug.LogError($"loopguard");
+                        break;
+                    }
+
+                    if (GetIndend(lines[index + 1]) <= archiveIndend)
+                        break;
+
+                    var trimmedLine = lines[index].Trim();
+                    if (trimmedLine.StartsWith("Bundle Dependencies", System.StringComparison.OrdinalIgnoreCase))
+                    {
+                        archive.bundleDependencies.AddRange(ReadBundleDependencies(ref index));
+                        continue;
+                    }
+
+                    if (trimmedLine.StartsWith("Expanded Bundle Dependencies", System.StringComparison.OrdinalIgnoreCase))
+                    {
+
+                    }
+
+                    if (trimmedLine.StartsWith("Explicit Assets", System.StringComparison.OrdinalIgnoreCase))
+                    {
+
+                    }
+
+                    if (trimmedLine.StartsWith("Files", System.StringComparison.OrdinalIgnoreCase))
+                    {
+
+                    }
+                }
+
+                return archive;
+            }
+
+            List<string> ReadBundleDependencies(ref int index)
+            {
+                var bundlesLine = lines[index];
+                bundlesLine = bundlesLine.Substring(bundlesLine.IndexOf("Bundle Dependencies:") + "Bundle Dependencies:".Length); // Remove everything before and including "Bundle Dependencies:"
+                bundlesLine = bundlesLine.Trim();
+
+                var bundles = new List<string>();
+                foreach (var b in bundlesLine.Split(new[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries))
+                    bundles.Add(b.Trim());
+
+                bundles.Sort();
+                return bundles;
+            }
+
+            int GetIndend(string s)
+            {
+                int count = 0;
+                for (var n = 0; n < s.Length; ++n)
+                {
+                    if (s[n] == '\t')
+                        count++;
+                    else
+                        break;
+                }
+                return count;
+            }
+        }
+    }
+
+}
---
+++ Editor/BuildLayoutWindow.cs
@@ -0,0 +1,92 @@
+﻿//
+// Addressables Build Layout Explorer for Unity. Copyright (c) 2021 Peter Schraut (www.console-dev.de). See LICENSE.md
+// https://github.com/pschraut/UnityAddressablesBuildLayoutExplorer
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Oddworm.EditorFramework
+{
+    public class BuildLayoutWindow : EditorWindow
+    {
+        BuildLayout m_Layout;
+
+        void OnEnable()
+        {
+            titleContent = new GUIContent("Build Layout Explorer");
+        }
+
+        void OnGUI()
+        {
+            DrawToolbar();
+        }
+
+        void DrawToolbar()
+        {
+            EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
+            if (GUILayout.Button("Load", EditorStyles.toolbarButton, GUILayout.Width(60)))
+            {
+                OpenLoadFileDialog();
+            }
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        void OpenLoadFileDialog()
+        {
+            var path = EditorUtility.OpenFilePanelWithFilters("Open BuildLayout.txt", "Assets/", new[] { "Text Files (*.txt)", "txt" });
+            if (string.IsNullOrEmpty(path))
+                return;
+            LoadBuildLayout(path);
+        }
+
+        void LoadBuildLayout(string path)
+        {
+            m_Layout = BuildLayout.Load(path);
+
+            var json = JsonUtility.ToJson(m_Layout, true);
+            System.IO.File.WriteAllText("Assets/BuildLayout.json", json);
+        }
+
+
+
+        [MenuItem("Window/Asset Management/Addressables/Build Layout Explorer", priority = 1000)]
+        static void OpenWindowMenuItem()
+        {
+            var wnd = GetWindow<BuildLayoutWindow>();
+            wnd.Show();
+        }
+
+        [MenuItem("Test/Read Layout")]
+        static void TestReadLayoutMenuItem()
+        {
+            var path = "Assets/buildlayout.txt";
+            var layout = BuildLayout.Load(path);
+            var json = JsonUtility.ToJson(layout, true);
+            System.IO.File.WriteAllText("Assets/BuildLayout.json", json);
+
+            foreach (var group in layout.groups)
+            {
+                var sb = new System.Text.StringBuilder();
+                sb.Append($"{group.name} = {group.size}\n");
+
+                foreach (var archive in group.bundles)
+                {
+                    sb.Append($"    Archive {archive.name} {archive.size}\n");
+
+                    if (archive.bundleDependencies.Count > 0)
+                        sb.Append($"      BundleDependencies {archive.bundleDependencies.Count}\n");
+
+                    foreach (var dep in archive.bundleDependencies)
+                    {
+                        sb.Append($"        {dep}\n");
+                    }
+                }
+
+                Debug.Log(sb.ToString());
+            }
+        }
+    }
+}
