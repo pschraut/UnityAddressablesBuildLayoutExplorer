df14cad783ecc90e42d6c03bf3a9c0c90e587d3e
pschraut <p-github@console-dev.de>
2025-12-22 09:05:51 +0100

    Updated year in copyright header

--- Editor/AssetDuplicatesView.cs
+++ Editor/AssetDuplicatesView.cs
@@ -1,8 +1,7 @@
 ﻿//
-// Addressables Build Layout Explorer for Unity. Copyright (c) 2021 Peter Schraut (www.console-dev.de). See LICENSE.md
+// Addressables Build Layout Explorer for Unity. Copyright (c) 2021-2025 Peter Schraut (www.console-dev.de). See LICENSE.md
 // https://github.com/pschraut/UnityAddressablesBuildLayoutExplorer
 //
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
